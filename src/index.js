b52681ed421250c088cef453aca0bf2bf6cbd11d
Alusor <eddvelez@outlook.com>
2019-01-30 12:48:26 -0600

    Configure entry point

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+import { createStackNavigator } from 'react-navigation'
+
+const App = () => (
+  <View/>
+);
+
+export default createStackNavigator({App});
