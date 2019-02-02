61390da6e2821b94daebc3f46444cf18b0fbf548
Alusor <eddvelez@outlook.com>
2019-02-01 23:44:18 -0600

    [add] buttons

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 import { SafeAreaView } from 'react-navigation';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 
 const Header = () => (
-  <SafeAreaView>
-    <Text style={{ fontSize: 44, fontWeight: 'bold', padding: 10 }}>Movies</Text>
+  <SafeAreaView style={{ flexDirection: 'row', alignItems: 'flex-end', padding: 10, justifyContent: 'space-between' }}>
+      <Text style={{ fontSize: 44, fontWeight: 'bold'}}>Movies</Text>
+      <View style={{ width: 50, justifyContent: 'space-between', flexDirection: 'row' }}>
+      <Icon name="md-search" size={20} style={{ marginBottom: 10 }}/>
+      <Icon name="logo-octocat" size={20} style={{ marginBottom: 10 }}/>
+      </View>
   </SafeAreaView>
 );
 
